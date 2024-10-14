8d3a309a08cb798d82476819f30c83e86889abed
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-14 10:21:07 -0300

    tela de carregamento + msg inicial

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -9,30 +9,41 @@ export const Main = () => {
 
     const [input, setInput] = useState('');
     const [listaTarefa, setListaTarefa] = useState<TarefaType[]>([]);
+    const [isLoading, setLoading] = useState(false);
 
     const VerificarInput = (input: string) => {
-        if (input.trim() !== '') return true
+        if (input.trim() !== '') return true;
     }
 
     const AddTarefa = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (!VerificarInput(input)) return
+        if (!VerificarInput(input)) return;
 
         if (event.key.toLocaleLowerCase() === 'enter') {
-            
+            setLoading(true);
             setListaTarefa([...listaTarefa, {
                 msg: input,
                 checkbox: false
             }]);
+
             setInput('');
-            alert("Tarefa adicionada!")
+            setInterval(() => {
+                setLoading(false);
+            }, 1800);
+            
         }
     }
 
     return (
         <div>
             <Input input={input} setInput={setInput} enter={AddTarefa}/>
+            {listaTarefa.length === 0 &&
+                <h2 className="text-center text-gray-400 text-md">Adicione uma tarefa para começar !!!</h2>
+            }
+            {isLoading &&
+                <h2 className="text-center text-gray-400 text-md">Carregando...</h2>
+            }
             <div className="flex flex-col gap-3">
-                {listaTarefa.map((item, index) => (
+                {!isLoading && listaTarefa.map((item, index) => (
                     <Tarefa item={item} key={index} listaTarefa={listaTarefa} setListaTarefa={setListaTarefa}/>
                 ))}  
             </div>
